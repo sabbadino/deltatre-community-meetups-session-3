9e73f192300dc4697345d1fdf7430980aa1516f0
Enrico Sabbadin <enrico.sabbadin@deltatre.it>
2020-05-29 16:45:04 +0200

    update slides and other few things

--- demo/aks-demoappdeploy-helm/installorupdatedemoapp.sh
+++ demo/aks-demoappdeploy-helm/installorupdatedemoapp.sh
@@ -1,3 +1,2 @@
-helm upgrade --debug --install k8sdemowithhelm helm/demoapp -f ./helm/myvalues.yaml
-
-# helm upgrade --debug --install k8sdemowithhelm demoapp -f ./myvalues-predemo.yaml
+helm upgrade --debug --install k8sdemowithhelm helm/demoapp 
+#-f ./helm/myvalues.yaml
